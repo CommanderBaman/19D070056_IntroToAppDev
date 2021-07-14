db11b95b1165d67f46556ca423a1459ee9197449
CommanderBaman <63144588+CommanderBaman@users.noreply.github.com>
2021-07-15 01:50:19 +0530

    complete state management

--- calculator/lib/main.dart
+++ calculator/lib/main.dart
@@ -10,12 +10,29 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Calculator',
-      home: Home(),
+      home: HomePage(),
     );
   }
 }
 
-class Home extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String queryString = '';
+  String answer = '';
+
+  void onNumpadPress(String charPressed) {
+    print(charPressed + ' asdflv');
+    setState(() {
+      queryString += charPressed;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,23 +46,68 @@ class Home extends StatelessWidget {
         children: <Widget>[
           Expanded(
             flex: 3,
-            child: Container(
-              color: Colors.blue,
+            child: AnswerWidget(
+              text: answer,
             ),
           ),
           Expanded(
             flex: 2,
-            child: Container(
-              color: Colors.green,
+            child: QueryWidget(
+              text: queryString,
             ),
           ),
           Expanded(
             flex: 10,
-            child: Container(
-              color: Colors.redAccent,
+            child: Numpad(
+              onKeyPress: onNumpadPress,
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class AnswerWidget extends StatelessWidget {
+  const AnswerWidget({Key? key, required this.text}) : super(key: key);
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      child: Text(this.text),
+    );
+  }
+}
+
+class QueryWidget extends StatelessWidget {
+  const QueryWidget({Key? key, required this.text}) : super(key: key);
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green,
+      child: Text(this.text),
+    );
+  }
+}
+
+class Numpad extends StatelessWidget {
+  const Numpad({Key? key, required this.onKeyPress}) : super(key: key);
+
+  final ValueSetter<String> onKeyPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      child: ElevatedButton(
+        child: Text('click me!'),
+        onPressed: () {
+          onKeyPress('sgg');
+        },
       ),
     );
   }
